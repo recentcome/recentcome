b5a99d53a2d44f76d838af2019b3c81a750573a1
recentcome <40929997+recentcome@users.noreply.github.com>
2019-03-05 15:01:18 +0800

    Update and rename selenlum to selenlum.py
    加入try判断"Message: Reached error page"网络异常后继续执行。

--- selenlum.py
+++ selenlum.py
@@ -3,6 +3,8 @@
 # @time     : 2019/3/4 16:05
 # @fILE     : selenlum.py
 # @Software : PyCharm
+# modify    :2019/3/5 8:30 by wangweipeng
+# 修改内容  : 加入try判断"Message: Reached error page"网络异常后继续执行。
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import os
@@ -32,9 +34,9 @@ for n in range(1,num):
         codexiangqing = driver.get(html)
         bspxiangqing = BeautifulSoup(driver.page_source, 'html.parser')
         spanxiangqing = bspxiangqing.select('.main-image a img')[0].get('src')
-        #获取详情页面的页数
+        # 获取详情页面的页数
         numbs = int(bspxiangqing.select('.pagenavi a')[-2].get_text())
-        j=1
+        j = 1
         # 开始下载图片
         if ": " in title:
             title = title.replace(': ', ' ')
@@ -42,22 +44,25 @@ for n in range(1,num):
         if not os.path.exists(title):
             os.mkdir(title)
             print('开始下载图片', title)
-            for j in range(1,numbs):
-                htmls = html + '/' + str(j)
-                codexiangqingnew = driver.get(htmls)
-                bspxiangqingnew = BeautifulSoup(driver.page_source, 'html.parser')
-                spanxiangqingnew = bspxiangqingnew.select('.main-image a img')[0].get('src')
+            for j in range(1, numbs):
+            #
+                try:
+                    htmls = html + '/' + str(j)
+                    codexiangqingnew = driver.get(htmls)
+                    bspxiangqingnew = BeautifulSoup(driver.page_source, 'html.parser')
+                    spanxiangqingnew = bspxiangqingnew.select('.main-image a img')[0].get('src')
 
-                # 存放到对应文件夹中
-                filename = '%s/%s/%s.jpg' % (os.path.abspath('.'), title, title+str(j))
-                headers = {
-                    'User-Agent': 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
-                    'Referer': 'http://www.mzitu.com/xinggan/'
-                }
-                response = requests.get(spanxiangqingnew, headers=headers)
-                with open(filename, 'wb') as img:
-                    img.write(response.content)
+                    # 存放到对应文件夹中
+                    filename = '%s/%s/%s.jpg' % (os.path.abspath('.'), title, title + str(j))
+                    headers = {
+                        'User-Agent': 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
+                        'Referer': 'http://www.mzitu.com/xinggan/'
+                    }
+                    response = requests.get(spanxiangqingnew, headers=headers)
+                    with open(filename, 'wb') as img:
+                        img.write(response.content)
+                except:
+                    continue
                 j = j + 1
     n = n+1
 print('====================The end================')
-
